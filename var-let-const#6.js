f72f8b789a508390979da997e7d5593cd26e926f
shaown <smshaown65@gmail.com>
2021-06-22 12:06:07 +0600

    var is done

---
+++ var-let-const#6.js
@@ -0,0 +1,25 @@
+// Var, Let, Const
+
+
+var name = "Jhon Doe"
+console.log(name);
+name = "Jhon Smith";
+console.log(name);
+
+// init Var
+var greeting;
+console.log(greeting);
+greeting ="Hello Friend";
+console.log(greeting);
+
+
+// Letter, Number, _, $
+//Con't Start With Number
+
+var $name ="";
+var _name = "";
+
+var firstName ='';  //Camel Case
+var first_name =''; //UnderScore
+var firstName = ''; //PasCale Case
+var firstname;
