705bc2ef584d4cb391ae24ca8e6c1e73a7c898b7
shaown <smshaown65@gmail.com>
2021-06-22 12:22:27 +0600

    var let const

--- var-let-const#6.js
+++ var-let-const#6.js
@@ -1,5 +1,6 @@
 // Var, Let, Const
 
+/*
 
 var name = "Jhon Doe"
 console.log(name);
@@ -23,3 +24,39 @@ var firstName ='';  //Camel Case
 var first_name =''; //UnderScore
 var firstName = ''; //PasCale Case
 var firstname;
+
+*/
+
+
+                    // Let
+/*
+let name = "shaown"
+console.log(name);
+name = "s m shaown"
+console.log(name);
+*/
+
+
+                //Const
+/*
+const name = 'Emon My Friend'
+console.log(name)
+
+// Can't Reassign
+// name = 'Emon My Friend'
+
+//Have to assign a valuel
+// const lastName;
+
+// exm..... 
+const person = {
+    name: 'Emon',
+    age: 30
+};
+console.log(person.name);
+
+person.name = 'shaown'
+console.log(person.name);
+*/
+
+
