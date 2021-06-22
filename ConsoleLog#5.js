584c378803ae76399b7fba306f7cadc67d6210b8
shaown <smshaown65@gmail.com>
2021-06-22 11:43:57 +0600

    conslone log codeing done

---
+++ ConsoleLog#5.js
@@ -0,0 +1,32 @@
+// log to console
+console.log("Hello World")
+console.log(123)
+console.log(true)
+
+//var 
+var greeting = "Hello There"
+console.log(greeting)
+
+//array
+console.log([1,2,3,4,5])
+
+//object
+console.table({a:1, b:2, c:3,})
+
+console.error("This is a Error Message");
+console.clear();
+
+console.warn("This is a waring")
+
+//Timer check
+console.time('Check')
+    console.log("Hello World");
+    console.log("Hello World");
+    console.log("Hello World");
+    console.log("Hello World");
+    console.log("Hello World");
+    console.log("Hello World");
+    console.log("Hello World");
+    console.log("Hello World");
+    console.log("Hello World");
+console.timeEnd('Check')
