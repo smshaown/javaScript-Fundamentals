3c81d53a79b08871f1c341c450b65b2e30766201
shaown <smshaown65@gmail.com>
2021-06-22 14:54:59 +0600

    data type

---
+++ #7-dataType.js
@@ -0,0 +1,49 @@
+//PRIMITIVE TYPE
+
+// String
+const name = 'Shaown';
+
+//Number
+const age = 22;
+
+//boolenl
+const hasKid = true;
+
+// Null 
+const car = null;
+
+//udefined
+let test;
+
+//Symbol
+const sym = Symbol();
+
+
+
+
+// REFERENCE TYPE
+
+// Array
+const array = ['array', 'object']
+/*
+    The typeof an array is an object. 
+    In Javascript , arrays are technically objects;
+    Just with special behaviours and abilities. For example,
+    Arrays have a array.prototype.length property.
+    which will return the number of elements in the array.
+
+*/
+
+
+// Object
+const objectAD = {
+    city: 'Mohangonj',
+    state: 'Markas'
+};
+
+
+//Date 
+const today = new Date();
+console.log(today)
+console.log(typeof today);
+
