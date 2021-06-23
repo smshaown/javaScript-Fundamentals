a39d3ab6796817b34dd957a4e8f068c35fc7f57b
shaown <smshaown65@gmail.com>
2021-06-23 14:26:54 +0600

    Switch case Statement js

--- #15-swithch-statement.js
+++ #15-swithch-statement.js
@@ -13,3 +13,29 @@ switch(color){
     default:
         console.log("color is not red, blue or black")
 }
+
+let day;
+switch(new Date().getDay()){
+    case 0:
+        day = 'Sunday';
+        break;
+    case 1:
+        day = 'Monday';
+        break;
+    case 2:
+        day = 'Tuesday';
+        break;
+    case 3:
+        day = 'Wednesday';
+        break;
+    case 4:
+        day = 'Thursday';
+        break;
+    case 5:
+        day = 'Friday';
+        break;
+    case 6:
+        day = 'Saturday';
+        break;
+}
+console.log(`Today is ${day}`)
