a41713552e2417148c44f1aaade2d4f69e0a94e3
shaown <smshaown65@gmail.com>
2021-06-23 06:40:05 +0600

    Array and Array Methods in JS

--- #12-arrayMethods.js
+++ #12-arrayMethods.js
@@ -34,11 +34,42 @@ numbers.unshift(120);
 numbers.unshift(1);
 
 // Take off From End
-
-
+numbers.pop();
 // Take off From Front
+numbers.shift();
 
 
+// Slice Value
+numbers.splice(1,3);
 
-console.log(numbers);
+// Reverse
+numbers.reverse();
+
+// Concatenate Array
+val = numbers.concat(numbers2);
+
+// Sorting Arrays
+val = fruits.sort();
+val = numbers.sort();
+
+//Use the 'compare function'
+val = numbers.sort(function(x,y){
+    return  x - y;
+})
+
+
+//Revers Sort
+val = numbers.sort(function(x,y){
+    return y - x;
+});
+
+// Find 
+function under50(element){
+    return element > 90;
+}
+
+val = numbers.find(under50);
+
+
+// console.log(numbers);
 console.log(val)
