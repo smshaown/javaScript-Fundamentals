219a0c7cfcd651b48ea2d277368e29c379c3c70e
shaown <smshaown65@gmail.com>
2021-06-23 08:39:02 +0600

    If else

--- #13-Object-literals.js
+++ #13-Object-literals.js
@@ -1,65 +1,60 @@
-// const person = {
-//     firstName: 's m',
+//if(something){
+//     do something
+// }else{
+//     do something else
 // }
 
-// let val;
-// val = person;
-// console.log(val)
+const id = 100;
 
 
-const person = {
-    firstName: 's m',
-    lastName: 'shaown',
-    age: 21,
-    email: 'smshaown26@gmail.com',
-    hobbies: ['sport', 'Travel', 'Nashid', 'Alquran'],
-    address: {
-        city: 'khulna',
-        state: 'Rupsa',
-        house: {
-            houseNo: '32',
-            street: '27 Islampur',
-            floor: '1st'
-        }
-    },
-    getBirthYear: function(){
-        return 2019 - this.age;
-    }
+
+//EQUAL to 
+// if(id == 100){
+//     console.log("CORRECT")
+// }else{
+//     console.log("INCORRECT")
+// }
+
+
+//not EQUAL to value
+// if(id != 100){
+//     console.log("Correct")
+// }else{
+//     console.log("Incorrect")
+// }
+
+
+
+
+
+
+// // EQUAL TO VALUE & TYPE
+// if(id === 100){
+//     console.log("correct")
+// }else{
+//     console.log("Incorrect")
+// }
+
+
+// NOT EQUAL TO VALUE & TYPE
+// if(id !== 100){
+//     console.log("correct")
+// }else{
+//     console.log("Incorrect")
+// }
+
+// TEST IF UNDEFINED
+// if(typeof id !== 'undefined'){
+//     console.log(`The ID id ${id}`)
+// }else{
+//     console.log('NI ID')
+// }
+
+
+//GREATER THAN LESS THAN
+if(id >= 90){
+    console.log("CORRECT");
+}else{
+    console.log("INCORRECT");
 }
 
-let val;
-val = person;
-
-//Get Specfic Value
-val = person.firstName;
-val = person['firstName'];
-
-val = person.age;
-
-val = person.email,
-val = person['email'],
-
-val = person.hobbies,
-val = person.hobbies[0],
-
-val = person.address;
-val = person.address.city;
-val = person.address.state;
-val = person.address.house.floor;
-
-val = person.getBirthYear();
-
-
-// console.log(val)
-
-const people = [
-    {name: 'shaown', age: 21},
-    {name: 'Emon', age: 22},
-    {name: 'Rakib', age: 22},
-    {name: 'Janu', age: 22},
-];
-
-for( i = 0; i<people.length; i++){
-    console.log(people[i].name)
-    console.log(people[i].age)
-}
---
+++ #14-if-else.js
@@ -0,0 +1,11 @@
+const person = {
+    firstName: 'shaown',
+}
+
+let val;
+
+// Get Specfic Value
+val = person.firstName;
+val = person['firstName'];
+
+console.log(val);
