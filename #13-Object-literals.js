1f290547300822ac55994842d5d5bdd94ee0da36
shaown <smshaown65@gmail.com>
2021-06-23 11:31:09 +0600

    If else js

--- #13-Object-literals.js
+++ #13-Object-literals.js
@@ -52,9 +52,56 @@ const id = 100;
 
 
 //GREATER THAN LESS THAN
-if(id >= 90){
-    console.log("CORRECT");
-}else{
-    console.log("INCORRECT");
-}
+// if(id >= 90){
+//     console.log("CORRECT");
+// }else{
+//     console.log("INCORRECT");
+// }
 
+
+// If Else Statement
+// const color = 'black'
+
+// if(color === 'red'){
+//     console.log("Color is red")
+// }else if(color === 'blue'){
+//     console.log("Color is blue")
+// }else if(color === 'black'){
+//     console.log("Color is black")
+// }else{
+//     console.log('color is not red, blue or black')
+// }
+
+
+
+
+// LOGICAL OPERATORS
+
+// // AND && 
+// const name = 'Jhon';
+// const age = 22;
+
+// if(age > 0 && age < 12){
+//     console.log(`${name} is child`)
+// }else if(age >= 13 && age <= 19){
+//     console.log(`${name} is Tennager`)
+// }else{
+//     console.log('${name} is Adult')
+// }
+
+//OR ||
+// if(age < 16 || age> 60){
+//     console.log(`${name} can not run in reace`)
+// }else{
+//     console.log(`${name} is registerd for the racel`)
+// }
+
+// Ternary OperAtor
+// console.log(id === 100 ? "CORRECT" : "INCORRECT")
+
+
+//WITHOUT BRACES
+if(id === 100)
+    console.log("CORRECT")
+ else
+    console.log("INCORRECT")
