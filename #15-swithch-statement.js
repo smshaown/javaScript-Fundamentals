356a13c68e47f7f04be83a0609238bd59c20129f
shaown <smshaown65@gmail.com>
2021-06-23 14:15:43 +0600

    Switch case start

---
+++ #15-swithch-statement.js
@@ -0,0 +1,15 @@
+const color = 'red';
+
+switch(color){
+    case 'blue':
+        console.log("Color is Blue");
+        break;
+    case 'red':
+        console.log("Color is Red");
+        break;
+    case 'black':
+        console.log("Color is Black");
+        break;
+    default:
+        console.log("color is not red, blue or black")
+}
